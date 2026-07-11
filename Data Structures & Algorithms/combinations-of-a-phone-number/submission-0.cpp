d9d27ce2356e9357faac8c862763b570fc6e5718
Ayush727363 <dwivediayush818@gmail.com>
2026-07-12 00:16:19 +0530

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> ans;
+    string temp = "";
+
+    void f(int it, string& dig, vector<string>& mp){
+        if(it == dig.size()){
+            ans.push_back(temp);
+            return;
+        }
+        for(char c : mp[dig[it] - '0']){
+            temp.push_back(c);
+            f(it+1, dig, mp);
+            temp.pop_back();
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if(digits == "") return {};
+        vector<string> mp = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        f(0, digits, mp);
+        return ans;
+    }
+};
