f95577c797b99ff71ad31208ff88174a307f6759
Ayush727363 <dwivediayush818@gmail.com>
2026-07-18 00:23:42 +0530

    Add: dota2-senate - submission-4

---
+++ Data Structures & Algorithms/dota2-senate/submission-4.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string predictPartyVictory(string s) {
+        int sr=0, sd=0;
+        int n = s.size();
+        queue<char> q;
+        for(char& c : s){
+            if(c == 'R') sr++;
+            else sd++;
+            q.push(c);
+        }
+        // if(sr == 0) return "Dire";
+        // else if(sd==0) return "Radiant";  
+        int r=0, d=0;
+        while(sr>0 && sd>0){
+            char t = q.front();
+            if(t == 'R'){
+                if(d > 0){
+                    d--;
+                    sr--;
+                    q.pop();
+                }else{
+                    r++;
+                    q.pop();
+                    q.push(t);
+                }
+            }else{
+                if(r > 0){
+                    r--;
+                    sd--;
+                    q.pop();
+                }else{
+                    d++;
+                    q.pop();
+                    q.push(t);
+                }
+            }
+        }
+        if(sr>0) return "Radiant";
+        else return "Dire";
+    }
+};
